0341bcb2feafd9b9e33b081ce11847f2873a952f
EiTGrupa2 <154069839+EiTGrupa2@users.noreply.github.com>
2024-02-04 17:35:01 +0100

    Update hx711_gain.h

--- kodzik/hx711_gain.h
+++ kodzik/hx711_gain.h
@@ -1,11 +1,11 @@
 void hx711init(uint8_t Gain){
-//Init pins
+//Piny Init
 PD_SCK_SET_AS_OUTPUT;
 PD_SCK_SET_LOW;
 DOUT_SET_AS_INPUT;
 DOUT_PULL_UP;
-//Set the channel
-//and the gain
-//for the next reading
+//Ustaw kanał
+//i wzmocnienie
+//dla kolejnego odczytu
 hx711read(Gain);
 }
