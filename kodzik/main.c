dd723e39f4c8bacea03d49f3853750dacc3c3659
EiTGrupa2 <154069839+EiTGrupa2@users.noreply.github.com>
2024-02-04 17:29:43 +0100

    Update main.c

--- kodzik/main.c
+++ kodzik/main.c
@@ -1,9 +1,22 @@
-void hx711init(uint8_t Gain){
-//Piny Init
-PD_SCK_SET_AS_OUTPUT;
+uint32_t hx711read(uint8_t Gain){
+uint32_t Value = 0;
+//Czeka na gotowość HX711
+while(!hx711isReady());
+//Odczytuje wszystkie 24 bity z MSB
+for(uint8_t i = 0; i < 24; ++i){
+Value <<= 1;
+PD_SCK_SET_HIGH;
+_delay_us(1);
+if(DOUT_READ) Value++;
 PD_SCK_SET_LOW;
-DOUT_SET_AS_INPUT;
-DOUT_PULL_UP;
-
-hx711read(Gain);
+_delay_us(1);
+}
+//Ustawia kanal i wzmocnienie dla nastepnego odczytu
+for (uint8_t i = 0; i < Gain; ++i){
+PD_SCK_SET_HIGH;
+_delay_us(1);
+PD_SCK_SET_LOW;
+_delay_us(1);
+}
+return Value;
 }
